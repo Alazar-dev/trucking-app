24f4aa72349e83615db5e28c9c2867124ea6c3d4
Alazar-dev <mealazartadesse@gmail.com>
2021-04-21 06:27:31 +0300

    added a moadl

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,12 +14,15 @@ import {
   View,
   Text,
   StatusBar,
-  Button,
+  Modal,
+  Alert,
+  Pressable,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const Home = ({navigation}) => {
+  const [modalVisible, setModalVisible] = useState(true);
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -28,7 +31,38 @@ const Home = ({navigation}) => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <View style={styles.body}>
-            <Text>Home</Text>
+            <View style={styles.centeredView}>
+              <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                  setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                    <Text style={styles.modalText}>
+                      Welcome to Trucking app
+                    </Text>
+                    <Text style={styles.paragraph}>
+                      To begin your service please enter details of your vehicle
+                    </Text>
+                    <View
+                      style={{
+                        borderBottomColor: '#000000',
+                        borderBottomWidth: 1,
+                        marginTop: 40,
+                      }}
+                    />
+                    <Pressable
+                      style={[styles.button, styles.buttonClose]}
+                      onPress={() => setModalVisible(!modalVisible)}>
+                      <Text style={styles.textStyle}>Continue</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </Modal>
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -72,6 +106,49 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    padding: 10,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    // backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: '#2196F3',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  modalText: {
+    fontSize: 22,
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  paragraph: {
+    textAlign: 'center',
   },
 });
 
