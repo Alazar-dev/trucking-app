354005d63a1763fbac42f13564898e11a8db370e
Alazar-dev <mealazartadesse@gmail.com>
2021-04-22 17:30:33 +0300

    added some styles

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+const styles = StyleSheet.create({
+  map: {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 425,
+  },
+});
 
 const Map = () => {
   return (
     <View>
       <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
