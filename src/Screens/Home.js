1dee51105237525966ed5f75dc9dc8d33a34cea7
Alazar-dev <mealazartadesse@gmail.com>
2021-04-22 10:54:11 +0300

    removed all modal files and rendered as a component itself

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -6,154 +6,20 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Modal,
-  Alert,
-  Pressable,
-  TouchableOpacity,
-} from 'react-native';
+import React from 'react';
+import {ScrollView, StatusBar} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import WelcomeModal from '../components/modal/ModalWelcome';
 
 const Home = (props) => {
-  const [modalVisible, setModalVisible] = useState(true);
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.centeredView}>
-              <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                  setModalVisible(!modalVisible);
-                }}>
-                <View style={styles.centeredView}>
-                  <View style={styles.modalView}>
-                    <Text style={styles.modalText}>
-                      Welcome to Trucking app
-                    </Text>
-                    <Text style={styles.paragraph}>
-                      To begin your service please enter details of your vehicle
-                    </Text>
-                    <View
-                      style={{
-                        borderBottomColor: '#000000',
-                        borderBottomWidth: 1,
-                        marginTop: 40,
-                      }}
-                    />
-                    <TouchableOpacity
-                      style={[styles.button, styles.buttonClose]}
-                      onPress={() =>
-                        props.navigation.navigate('VehicleDetail') &&
-                        setModalVisible(!modalVisible)
-                      }>
-                      <Text style={styles.textStyle}>Continue</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </Modal>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <WelcomeModal />
+      </ScrollView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    padding: 10,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    // backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: '#2196F3',
-    fontSize: 25,
-    textAlign: 'center',
-  },
-  modalText: {
-    fontSize: 22,
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-  paragraph: {
-    textAlign: 'center',
-  },
-});
 
 export default Home;
