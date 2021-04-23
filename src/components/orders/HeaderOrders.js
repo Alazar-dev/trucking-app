696cb6f7184567c21519dfd5ad1adb65ff528964
Alazar-dev <mealazartadesse@gmail.com>
2021-04-23 19:31:10 +0300

    improved content

--- src/components/orders/HeaderOrders.js
+++ src/components/orders/HeaderOrders.js
@@ -1,45 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Picker} from '@react-native-community/picker';
 
 const HeaderOrders = () => {
-  const [selectedValue, setSelectedValue] = useState('java');
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          flexDirection: 'row',
-        },
-      ]}>
-      <View style={{flex: 6}} />
-      <View style={{flex: 9}}>
-        <Text style={styles.topText}>Earnings</Text>
-        <View>
-          <Picker
-            selectedValue={selectedValue}
-            style={{height: 50, width: 300}}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedValue(itemValue)
-            }>
-            <Picker.Item label="GMC Suburban" value="gmcsuburban" />
-            <Picker.Item label="Toyota V8" value="toyotav8" />
-          </Picker>
-        </View>
-      </View>
-      <View style={{flex: 5}}>
-        <Text>Send Invoice</Text>
-      </View>
+    <View>
+      <Text style={styles.topText}>History</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-  },
   topText: {
+    padding: '20',
     fontSize: 20,
+    textAlign: 'center',
   },
 });
 export default HeaderOrders;
