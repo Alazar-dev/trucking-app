de07e8d509f72f51092084b5a9a280b9be8a7adf
Alazar-dev <mealazartadesse@gmail.com>
2021-04-23 19:32:44 +0300

    added content

--- src/Screens/main/Settings.js
+++ src/Screens/main/Settings.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import {ScrollView, Text} from 'react-native';
-import {Container, Toolbar} from 'components';
+import {
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {Avatar} from 'react-native-elements';
+import {Container} from 'components';
 
 import HeaderSetting from '../../components/settings/HeaderSetting';
 
-const Profile = () => {
+const Setting = () => {
   return (
     <Container>
       <ScrollView>
         <HeaderSetting />
-        <Text>Setting</Text>
+        <TouchableOpacity style={styles.onlineStatus}>
+          <Text style={styles.buttonText}>ONLINE</Text>
+        </TouchableOpacity>
+        <View>
+          <Avatar rounded title="MD" />
+        </View>
       </ScrollView>
     </Container>
   );
 };
 
-export default Profile;
+const styles = StyleSheet.create({
+  onlineStatus: {
+    backgroundColor: '#20b2aa',
+  },
+  buttonText: {
+    padding: 10,
+    color: '#fff',
+  },
+});
+
+export default Setting;
