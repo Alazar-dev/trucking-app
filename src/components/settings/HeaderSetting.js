adad892ebe352cfdcea7c851c7246b2ae9e9342d
Alazar-dev <mealazartadesse@gmail.com>
2021-04-29 19:04:26 +0300

    added clock and online status

--- src/components/settings/HeaderSetting.js
+++ src/components/settings/HeaderSetting.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Clock from './Clock';
 
 const HeaderSettings = () => {
   return (
-    <View style={styles.container}>
-      <View style={{flex: 6}} />
-      <View style={{flex: 9}}>
-        <Text style={styles.topText}>Setting</Text>
+    <View>
+      <View style={styles.container}>
+        <View style={{flex: 6}} />
+        <View style={{flex: 9}}>
+          <Text style={styles.topText}>Setting</Text>
+        </View>
+      </View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 9}}>
+          <TouchableOpacity style={styles.onlineStatus}>
+            <Text style={styles.buttonText}>ONLINE</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 3, height: '1%'}}>
+          <TouchableOpacity style={styles.onlineStatus}>
+            <Clock />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -19,6 +34,13 @@ const styles = StyleSheet.create({
   },
   topText: {
     fontSize: 20,
+  },
+  onlineStatus: {
+    backgroundColor: '#20b2aa',
+  },
+  buttonText: {
+    padding: 10,
+    color: '#fff',
   },
 });
 export default HeaderSettings;
