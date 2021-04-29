a02b754e8e589ea17840cf8fd749d47f65919a59
Alazar-dev <mealazartadesse@gmail.com>
2021-04-29 12:56:21 +0300

    rendered data table

--- src/Screens/main/Orders.js
+++ src/Screens/main/Orders.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import {ScrollView, View, Text} from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import HeaderOrders from '../../components/orders/HeaderOrders';
+import DataTable from '../../components/orders/DataTable';
 
 const Orders = () => {
   return (
     <View>
       <ScrollView>
         <HeaderOrders />
-        <Text>Orders</Text>
+        <TouchableOpacity style={styles.onlineStatus}>
+          <Text style={styles.buttonText}>Today</Text>
+        </TouchableOpacity>
+        <DataTable />
       </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  onlineStatus: {
+    backgroundColor: '#c9cdd4',
+  },
+  buttonText: {
+    padding: 10,
+    color: '#000',
+  },
+});
 
 export default Orders;
