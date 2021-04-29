408195fb7c3375baf1d68910b92f2578dff6979a
Alazar-dev <mealazartadesse@gmail.com>
2021-04-29 19:05:25 +0300

    removed and rendered from their own component

--- src/Screens/main/Settings.js
+++ src/Screens/main/Settings.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import {ScrollView, TouchableOpacity, Text, StyleSheet} from 'react-native';
-import {Container} from 'components';
+import {ScrollView, View} from 'react-native';
+import Profile from '../../components/settings/Profile';
 
 import HeaderSetting from '../../components/settings/HeaderSetting';
 
 const Setting = () => {
   return (
-    <Container>
+    <View>
       <ScrollView>
         <HeaderSetting />
-        <TouchableOpacity style={styles.onlineStatus}>
-          <Text style={styles.buttonText}>ONLINE</Text>
-        </TouchableOpacity>
+        <Profile />
       </ScrollView>
-    </Container>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  onlineStatus: {
-    backgroundColor: '#20b2aa',
-  },
-  buttonText: {
-    padding: 10,
-    color: '#fff',
-  },
-});
 
 export default Setting;
