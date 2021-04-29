5a14e97c0af50057bbdee719e7193bf0f1f82ce8
Alazar-dev <mealazartadesse@gmail.com>
2021-04-29 08:00:09 +0300

    imporoved code structure

--- src/components/home/HeaderHome.js
+++ src/components/home/HeaderHome.js
@@ -1,10 +1,8 @@
-import React, {useState} from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Badge} from 'react-native-elements';
+import {Ionicons} from '@expo/vector-icons';
 
 const HeaderHome = () => {
-  const [selectedValue, setSelectedValue] = useState('java');
   return (
     <View
       style={[
@@ -14,34 +12,13 @@ const HeaderHome = () => {
         },
       ]}>
       <View style={{flex: 6}}>
-        <Icon name="comment" size={40} color="#20b2aa" />
-        <Badge
-          value="3"
-          status="error"
-          containerStyle={{position: 'absolute', top: 10, right: 75}}
-        />
+        <Ionicons name="md-checkmark-circle" size={24} color="black" />
       </View>
       <View style={{flex: 9}}>
         <Text style={styles.topText}>GMC Suburban</Text>
-        <View>
-          {/* <Picker
-            selectedValue={selectedValue}
-            style={{height: 50, width: 300}}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedValue(itemValue)
-            }>
-            <Picker.Item label="GMC Suburban" value="gmcsuburban" />
-            <Picker.Item label="Toyota V8" value="toyotav8" />
-          </Picker> */}
-        </View>
       </View>
       <View style={{flex: 2}}>
-        <Icon name="bell" size={40} color="#000000" />
-        <Badge
-          value="3"
-          status="error"
-          containerStyle={{position: 'absolute', top: 10, right: 0}}
-        />
+        <Ionicons name="md-checkmark-circle" size={24} color="black" />
       </View>
     </View>
   );
