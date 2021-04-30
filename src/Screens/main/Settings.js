5a07b3cfd328a48dc0382ac8d6b50b69a72b907c
Alazar-dev <mealazartadesse@gmail.com>
2021-04-30 11:30:40 +0300

    added props

--- src/Screens/main/Settings.js
+++ src/Screens/main/Settings.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
-import Profile from '../../components/settings/Profile';
+import Profile from '../../components/settings/profile/Profile';
 import ProfileDetail from '../../components/settings/ProfileDetail';
 
 import HeaderSetting from '../../components/settings/HeaderSetting';
 
-const Setting = () => {
+const Setting = (props) => {
   return (
     <View>
       <ScrollView>
         <HeaderSetting />
-        <Profile />
+        <Profile navigation={props.navigation} />
         <ProfileDetail />
       </ScrollView>
     </View>
