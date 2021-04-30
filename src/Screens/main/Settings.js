1d827e7c59aae89ff624c0a8fc356938dae4e383
Alazar-dev <mealazartadesse@gmail.com>
2021-04-30 15:41:28 +0300

    rendered profile detail

--- src/Screens/main/Settings.js
+++ src/Screens/main/Settings.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
 import Profile from '../../components/settings/profile/Profile';
-import ProfileDetail from '../../components/settings/ProfileDetail';
+import ProfileDetail from '../../components/settings/profile/ProfileDetail';
 
 import HeaderSetting from '../../components/settings/HeaderSetting';
 
-const Setting = (props) => {
+const Settings = (props) => {
   return (
     <View>
       <ScrollView>
         <HeaderSetting />
         <Profile navigation={props.navigation} />
-        <ProfileDetail />
+        <ProfileDetail navigation={props.navigation} />
       </ScrollView>
     </View>
   );
 };
 
-export default Setting;
+export default Settings;
