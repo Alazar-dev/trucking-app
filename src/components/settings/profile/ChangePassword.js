68d4ee7f7dd5e4786dd878da023a067078b62087
Alazar-dev <mealazartadesse@gmail.com>
2021-04-30 11:28:29 +0300

    created change password component

---
+++ src/components/settings/profile/ChangePassword.js
@@ -0,0 +1,123 @@
+import React, {useState, createRef} from 'react';
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+} from 'react-native';
+
+const ProfileEdit = (props) => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const passwordInputRef = createRef();
+
+  return (
+    <View style={styles.mainBody}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <View>
+          <KeyboardAvoidingView enabled>
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.textHeader}>My Account</Text>
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={() => setName(name)}
+                placeholder="NAME"
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current && passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={() => setLastName(lastName)}
+                placeholder="LAST NAME"
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current && passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={() => setPhone(phone)}
+                setPhone
+                placeholder="PHONE"
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current && passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate('Setting')}
+              style={styles.buttonStyle}
+              activeOpacity={0.5}>
+              <Text style={styles.changePassword}>Change Password</Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  textHeader: {
+    fontSize: 20,
+    padding: 20,
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+    borderBottomWidth: 0.3,
+  },
+  inputStyle: {
+    flex: 1,
+    color: '#000000',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  changePassword: {
+    color: '#000',
+    fontSize: 25,
+    marginLeft: 35,
+    fontWeight: '100',
+  },
+});
+
+export default ProfileEdit;
