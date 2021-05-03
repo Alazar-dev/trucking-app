4f139ce88b0cb0dd7819a7cf5cd252daac9728f3
Alazar-dev <mealazartadesse@gmail.com>
2021-05-03 20:40:12 +0300

    removed unnecessary comments

--- src/components/earnings/HeaderEarnings.js
+++ src/components/earnings/HeaderEarnings.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 // import {Picker} from '@react-native-community/picker';
 
-const HeaderEarnings = () => {
+const HeaderEarnings = (props) => {
   const [selectedValue, setSelectedValue] = useState('java');
   return (
     <View
@@ -15,20 +15,11 @@ const HeaderEarnings = () => {
       <View style={{flex: 6}} />
       <View style={{flex: 9}}>
         <Text style={styles.topText}>Earnings</Text>
-        <View>
-          {/* <Picker
-            selectedValue={selectedValue}
-            style={{height: 50, width: 300}}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedValue(itemValue)
-            }>
-            <Picker.Item label="GMC Suburban" value="gmcsuburban" />
-            <Picker.Item label="Toyota V8" value="toyotav8" />
-          </Picker> */}
-        </View>
       </View>
       <View style={{flex: 5}}>
-        <Text>Send Invoice</Text>
+        <TouchableOpacity onPress={() => props.navigation.navigate('Invoices')}>
+          <Text>Send Invoice</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
