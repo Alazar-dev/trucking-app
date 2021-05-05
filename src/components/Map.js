c2b5ea40e2d4ff35c7172b6e1dc6c528db36a8e0
Alazar-dev <mealazartadesse@gmail.com>
2021-05-05 11:02:32 +0300

    added data picker

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,25 +4,43 @@ import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const styles = StyleSheet.create({
   map: {
-    height: '82%',
+    height: '83%',
   },
 });
 
 const Map = () => {
   return (
-    <View>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
+    <>
+      <View>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          loadingEnabled={true}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+      <MapView.Marker
+        coordinate={{
           latitude: 37.78825,
           longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
         }}
+        title={'Title 1'}
+        description={'Description 1'}
       />
-    </View>
+      <MapView.Marker
+        coordinate={{
+          latitude: 37.78925,
+          longitude: -122.4324,
+        }}
+        title={'Title 2'}
+        description={'Description 2'}
+      />
+    </>
   );
 };
-
 export default Map;
