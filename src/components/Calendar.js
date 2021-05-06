5f500c1a783c3cec0eadd8f633de25ba69feda54
Alazar-dev <mealazartadesse@gmail.com>
2021-05-06 17:26:23 +0300

    changed calendar to date picker

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,40 +1,65 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import CalendarPicker from 'react-native-calendar-picker';
+import React, {useState} from 'react';
 
-export default class Calendar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedStartDate: null,
-    };
-    this.onDateChange = this.onDateChange.bind(this);
-  }
+// import all the components we are going to use
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 
-  onDateChange(date) {
-    this.setState({
-      selectedStartDate: date,
-    });
-  }
-  render() {
-    const {selectedStartDate} = this.state;
-    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
-    return (
+//import DatePicker from the package we installed
+import DatePicker from 'react-native-datepicker';
+
+const App = () => {
+  const [date, setDate] = useState('09-10-2020');
+
+  return (
+    <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <CalendarPicker onDateChange={this.onDateChange} />
-
-        <View>
-          <Text>SELECTED DATE:{startDate}</Text>
-        </View>
+        <DatePicker
+          style={styles.datePickerStyle}
+          date={date} //initial date from state
+          mode="date" //The enum of date, datetime and time
+          placeholder="select date"
+          format="DD-MM-YYYY"
+          minDate="01-01-2016"
+          maxDate="01-01-2019"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              //display: 'none',
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0,
+            },
+            dateInput: {
+              marginLeft: 36,
+            },
+          }}
+          onDateChange={(date) => {
+            setDate(date);
+          }}
+        />
       </View>
-    );
-  }
-}
+    </SafeAreaView>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    marginTop: 100,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  datePickerStyle: {
+    width: 200,
+    marginTop: 20,
   },
 });
