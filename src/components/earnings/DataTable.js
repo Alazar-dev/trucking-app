ad9c499c54ce02b82689da2c0d8d331050921477
Alazar-dev <mealazartadesse@gmail.com>
2021-05-07 14:53:58 +0300

    improved UI

--- src/components/earnings/DataTable.js
+++ src/components/earnings/DataTable.js
@@ -8,7 +8,9 @@ export default function DataTable(props) {
   return (
     <View>
       {history.map((total) => (
-        <View key={total.id}>
+        <View
+          style={{borderBottomWidth: 1, borderBottomColor: '#51515151'}}
+          key={total.id}>
           <TouchableOpacity
             onPress={() => props.navigation.navigate('EarningsDetail')}
             style={{
@@ -33,9 +35,7 @@ export default function DataTable(props) {
       ))}
       <Picker
         selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedLanguage(itemValue)
-        }>
+        onValueChange={(itemValue) => setSelectedLanguage(itemValue)}>
         <Picker.Item label="Java" value="java" />
         <Picker.Item label="JavaScript" value="js" />
       </Picker>
@@ -44,13 +44,13 @@ export default function DataTable(props) {
 }
 
 const history = [
-  {id: 1, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 2, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 3, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 4, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 5, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 6, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 7, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 8, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
-  {id: 9, date: 'Monday 12', trip: '2 trips', earn: '$234.00'},
+  {id: 1, date: 'Monday 12', trip: '2 trips', earn: '$200.00'},
+  {id: 2, date: 'Sunday 10', trip: '1 trip', earn: '$150.00'},
+  {id: 3, date: 'Saturday 15', trip: '3 trips', earn: '$156.00'},
+  {id: 4, date: 'Monday 12', trip: '2 trips', earn: '$99.00'},
+  {id: 5, date: 'Friday 12', trip: '5 trips', earn: '$300.00'},
+  {id: 6, date: 'Thursday 12', trip: '4 trips', earn: '$234.00'},
+  {id: 7, date: 'wednesday 9', trip: '1 trips', earn: '$200.00'},
+  {id: 8, date: 'Saturday 15', trip: '1 trips', earn: '$299.00'},
+  {id: 9, date: 'Monday 12', trip: '1 trips', earn: '$305.00'},
 ];
